08cca5f53ebe0d109a629dabc65cfa4e3b44ab0d
Patuco <patricia.beco.dg@gmail.com>
2023-03-23 09:47:38 +0100

    Add detail page styles

--- src/components/CharDetail.js
+++ src/components/CharDetail.js
@@ -2,28 +2,38 @@ import { Link } from "react-router-dom";
 
 function CharDetail({ characterFind }) {
   return (
-    <div>
-      <Link to="/">
-        <button>Volver atrás</button>
-      </Link>
-      <img
-        className="char-det__img"
-        src={
-          characterFind.image
-            ? characterFind.image
-            : "https://via.placeholder.com/210x295/ffffff/666666/?text=HarryPotter"
-        }
-        alt={characterFind.name}
-      />
-      <h3 className="char-det__name">{characterFind.name}</h3>
-      <p className="char-det__text">{characterFind.aka}</p>
-      <p className="char-det__text">House of {characterFind.house}</p>
-      <p className="char-det__text">{characterFind.gender}</p>
-      <p className="char-det__text">Specie: {characterFind.species}</p>
-      <p className="char-det__text">
-       Status: {characterFind.alive === true ? "❤️" : "💀"}
-      </p>
-    </div>
+      <section className="char-det">
+        <img
+          className="char-det__img"
+          src={
+            characterFind.image
+              ? characterFind.image
+              : "https://via.placeholder.com/210x295/ffffff/666666/?text=HarryPotter"
+          }
+          alt={characterFind.name}
+        />
+        <h3 className="char-det__name">{characterFind.name}</h3>
+        <section className="char-det__info">
+          <p className="char-det__text char-det__text--aka">
+            {characterFind.aka[0]} / {characterFind.aka[1]}
+          </p>
+          <p className="char-det__text char-det__text--house">
+            House of {characterFind.house}
+          </p>
+          <p className="char-det__text char-det__text--gender">
+            Gender: {characterFind.gender}
+          </p>
+          <p className="char-det__text char-det__text--species">
+            Specie: {characterFind.species}
+          </p>
+          <p className="char-det__text char-det__text--status">
+            Status: {characterFind.alive === true ? "❤️" : "💀"}
+          </p>
+          <Link to="/">
+            <button className="char-det__btn">Volver atrás</button>
+          </Link>
+        </section>
+      </section>
   );
 }
 
